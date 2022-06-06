98030f1b6f2fc16f788858a4278d4a62e4a097fd
xstra <xstra@naver.com>
2022-06-06 16:21:42 +0900

    This code is first version that save lotto numbers. 2022.06.06

--- app/src/main/java/com/xstra/lottery/MainActivity.java
+++ app/src/main/java/com/xstra/lottery/MainActivity.java
@@ -3,13 +3,13 @@ package com.xstra.lottery;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+
 import android.widget.TextView;
+
 import com.airbnb.lottie.LottieAnimationView;
 
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -23,10 +23,19 @@ public class MainActivity extends AppCompatActivity {
     TextView n4 = findViewById( R.id.number4 );
     TextView n5 = findViewById( R.id.number5 );
     TextView n6 = findViewById( R.id.number6 );
-    int[] random =new int[6];
-    String[] num = new String[6];
+    TextView[] save = new TextView[6];
+
+    save[0] = findViewById( R.id.save1 );
+    save[1] = findViewById( R.id.save2 );
+    save[2] = findViewById( R.id.save3 );
+    save[3] = findViewById( R.id.save4 );
+    save[4] = findViewById( R.id.save5 );
+    save[5] = findViewById( R.id.save6 );
+
+    final int[] random =new int[6];
 
     LottieAnimationView lotteryButton = findViewById( R.id.lotteryButton );
+    LottieAnimationView saveButton = findViewById( R.id.saveButton );
     ArrayList<TextView> Numbers = new ArrayList<>( Arrays.asList( n1, n2, n3, n4, n5, n6 ) );
 //    HashSet<TextView> Numbers = new HashSet<>(Arrays.asList( n1, n2, n3, n4, n5, n6 ));
     final CountDownTimer countDownTimer = new CountDownTimer( 2000, 100 ) {
@@ -44,18 +53,44 @@ public class MainActivity extends AppCompatActivity {
                             i--;
                         }
                     }
-                    Numbers.get( i ).setText(String.valueOf(random[i]));
+                    Numbers.get(i).setText(String.valueOf(random[i]));
+       //             saveList.add(String.valueOf(random[i]));
                 }
-            };
+
+            }
         };
         lotteryButton.setOnClickListener( view -> {
-            if (lotteryButton.isAnimating()) {
-                lotteryButton.cancelAnimation();
-                countDownTimer.cancel();
-            } else {
+           // if (lotteryButton.isAnimating()) {
+           //     lotteryButton.cancelAnimation();
+           //     countDownTimer.cancel();
+            //} else {
                 lotteryButton.playAnimation();
                 countDownTimer.start();
+            //}
+        } );
+        saveButton.setOnClickListener( view -> {
+
+            for (int i = 0; i < save.length; i++) {
+                save[i].setText((Integer.toString(random[i])));
             }
+
+            //            lottoList.setText( "" );
+//            String save1 = Numbers.toString();
+//            saveButton.playAnimation();
+   //          for (int i = 0; i < 6; i++) {
+ //                 lottoList.append( save1 + " " );
+   //         }
+//
+            // else if (count == 2 || save2[0].getText().toString().equals( "" )) {
+             //   for (int i = 0; i < save.length; i++) {
+             //       save2[i].setText( (Integer.toString( temp[i] )) );
+             //   }
+          //  } else if (count == 3 || save3[0].getText().toString().equals( "" )) {
+          //      for (int i = 0; i < save.length; i++) {
+          //          save3[i].setText( (Integer.toString( temp[i] )) );
+          //      }
+          //      Toast.makeText( MainActivity.this, "Beginner's luck", Toast.LENGTH_SHORT ).show();
+          //  }
         } );
     }
 }
