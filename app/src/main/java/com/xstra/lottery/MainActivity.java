a703c91bc923a64458fdd4655010bdd0b020fd82
xstra <xstra@naver.com>
2022-06-06 22:57:18 +0900

    This code is second version that save & Reset lotto numbers. 2022.06.07

--- app/src/main/java/com/xstra/lottery/MainActivity.java
+++ app/src/main/java/com/xstra/lottery/MainActivity.java
@@ -1,6 +1,7 @@
 package com.xstra.lottery;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.os.CountDownTimer;
 
@@ -23,19 +24,29 @@ public class MainActivity extends AppCompatActivity {
     TextView n4 = findViewById( R.id.number4 );
     TextView n5 = findViewById( R.id.number5 );
     TextView n6 = findViewById( R.id.number6 );
-    TextView[] save = new TextView[6];
+    TextView[] save1 = new TextView[6];
+    TextView[] save2 = new TextView[6];
 
-    save[0] = findViewById( R.id.save1 );
-    save[1] = findViewById( R.id.save2 );
-    save[2] = findViewById( R.id.save3 );
-    save[3] = findViewById( R.id.save4 );
-    save[4] = findViewById( R.id.save5 );
-    save[5] = findViewById( R.id.save6 );
+    save1[0] = findViewById( R.id.save11 );
+    save1[1] = findViewById( R.id.save12 );
+    save1[2] = findViewById( R.id.save13 );
+    save1[3] = findViewById( R.id.save14 );
+    save1[4] = findViewById( R.id.save15 );
+    save1[5] = findViewById( R.id.save16 );
+
+    save2[0] = findViewById( R.id.save21 );
+    save2[1] = findViewById( R.id.save22 );
+    save2[2] = findViewById( R.id.save23 );
+    save2[3] = findViewById( R.id.save24 );
+    save2[4] = findViewById( R.id.save25 );
+    save2[5] = findViewById( R.id.save26 );
 
     final int[] random =new int[6];
+        final int[] count = {0};
 
     LottieAnimationView lotteryButton = findViewById( R.id.lotteryButton );
     LottieAnimationView saveButton = findViewById( R.id.saveButton );
+    LottieAnimationView resetButton = findViewById( R.id.resetButton );
     ArrayList<TextView> Numbers = new ArrayList<>( Arrays.asList( n1, n2, n3, n4, n5, n6 ) );
 //    HashSet<TextView> Numbers = new HashSet<>(Arrays.asList( n1, n2, n3, n4, n5, n6 ));
     final CountDownTimer countDownTimer = new CountDownTimer( 2000, 100 ) {
@@ -54,9 +65,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                     Numbers.get(i).setText(String.valueOf(random[i]));
-       //             saveList.add(String.valueOf(random[i]));
                 }
-
             }
         };
         lotteryButton.setOnClickListener( view -> {
@@ -69,29 +78,23 @@ public class MainActivity extends AppCompatActivity {
             //}
         } );
         saveButton.setOnClickListener( view -> {
-
-            for (int i = 0; i < save.length; i++) {
-                save[i].setText((Integer.toString(random[i])));
+            count[0] = count[0] + 1;
+            if (count[0] == 1 || save1[0].getText().toString().equals( "" )) {
+                for (int i = 0; i < save1.length; i++) {
+                save1[i].setText( (Integer.toString( random[i] )) );
             }
-
-            //            lottoList.setText( "" );
-//            String save1 = Numbers.toString();
-//            saveButton.playAnimation();
-   //          for (int i = 0; i < 6; i++) {
- //                 lottoList.append( save1 + " " );
-   //         }
-//
-            // else if (count == 2 || save2[0].getText().toString().equals( "" )) {
-             //   for (int i = 0; i < save.length; i++) {
-             //       save2[i].setText( (Integer.toString( temp[i] )) );
-             //   }
-          //  } else if (count == 3 || save3[0].getText().toString().equals( "" )) {
-          //      for (int i = 0; i < save.length; i++) {
-          //          save3[i].setText( (Integer.toString( temp[i] )) );
-          //      }
-          //      Toast.makeText( MainActivity.this, "Beginner's luck", Toast.LENGTH_SHORT ).show();
-          //  }
+            } else if (count[0] == 2 || save2[0].getText().toString().equals( "" )) {
+                for (int i = 0; i < save2.length; i++) {
+                    save2[i].setText( (Integer.toString( random[i] )) );
+                }
+            }
         } );
+        resetButton.setOnClickListener( view -> {
+            for (int i = 0; i < save1.length; i++) {
+                save1[i].setText(null);
+                save2[i].setText(null);
+            }
+        });
     }
 }
 
